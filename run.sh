23206ffd328895451ac51b50ba977bf6dbfbb665
猫科龙 <974985526@qq.com>
2019-03-21 15:49:30 +0800

    扩展：run.sh 可以顺便编译 dbg.cpp 了
    另外发现刚克隆本仓储时无法生成 pintool，原因是需要手动生成保存中间文件的文件夹。

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ CONFIG_SRC_WPMFS="https://github.com/maokelong/wpmfs"
 
 CONFIG_PATH_PINTOOL="wpmfs-pintool.cpp"
 CONFIG_PATH_OUTPUT="output"
+CONFIG_PATH_DBG="dbg.cpp" # cpp, sse2
 
 # 如果没有解压 pin，就解压 pin
 if [[ ! -d $(basename $CONFIG_SRC_PIN .tar.gz) ]]; then
@@ -20,12 +21,13 @@ fi
 # 拷贝 pintool 到 pin 的特定目录并 make
 cp $CONFIG_PATH_PINTOOL $(basename $CONFIG_SRC_PIN .tar.gz)/source/tools/MyPinTool
 pushd $(basename $CONFIG_SRC_PIN .tar.gz)/source/tools/MyPinTool
+mkdir -p obj-intel64
 make obj-intel64/$(basename $CONFIG_PATH_PINTOOL .cpp).so
 popd
 
 # 如果没有 mount wpmfs，就 mount wpmfs
-if [[ ! $(lsmod | grep wpmfs) ]];then
-  if [[ ! -d wpmfs ]];then
+if [[ ! $(lsmod | grep wpmfs) ]]; then
+  if [[ ! -d wpmfs ]]; then
     git clone https://github.com/maokelong/wpmfs wpmfs
   fi
   pushd wpmfs
@@ -35,6 +37,9 @@ fi
 
 # TODO-MKL: 安装配置 whipser
 
+# 生成测试用应用
+g++ $CONFIG_PATH_DBG -msse2 -o "${CONFIG_PATH_DBG%.cpp}.out"
+
 # 使用生成的 pintool 插桩应用
 if [[ ! -d $CONFIG_PATH_OUTPUT ]]; then
   mkdir -p $CONFIG_PATH_OUTPUT
@@ -43,4 +48,4 @@ $(basename $CONFIG_SRC_PIN .tar.gz)/pin \
   -t $(basename $CONFIG_SRC_PIN .tar.gz)/source/tools/MyPinTool/obj-intel64/$(basename $CONFIG_PATH_PINTOOL .cpp).so \
   -logfile "${CONFIG_PATH_OUTPUT}/pin.log" \
   -o "${CONFIG_PATH_OUTPUT}/pin.output" \
-  -- ./a.out
+  -- ./"${CONFIG_PATH_DBG%.cpp}.out"
